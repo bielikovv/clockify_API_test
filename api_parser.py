acff6234f2b20a52ef832100841c7b523cd84570
dima <shvachko319@gmail.com>
2023-03-07 19:37:32 +0100

    Шеф не затримуй зарплату

--- api_parser.py
+++ api_parser.py
@@ -6,12 +6,18 @@ import datetime
 class Cat:
     name = None
     age = None
+    weight = None
+    happy = None
 
-    def __init__(self, name, age) -> None:
+    def __init__(self, name, age, weight, happy) -> None:
         self.name = name
         self.age = age
+        self.weight = weight
+        self.happy = happy
+
+    #Начальник не затримуй зарплату
     def get_data(self):
-        return self.name, self.age
+        return self.name, self.age, self.weight, self.happy
 
 
 X_API_KEY = 'CLOCKIFY_API_KEY'
